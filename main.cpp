b9cf5ad610bf67f1b8755712d3784641193ccbbb
Hanner Delgado <hdelgadoe@est.ups.edu.ec>
2022-07-18 09:30:10 -0500

    Nombre

---
+++ main.cpp
@@ -0,0 +1,13 @@
+//Hanner Delgado
+//1729279123
+#include "juego.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Juego w;
+    w.show();
+    return a.exec();
+}
